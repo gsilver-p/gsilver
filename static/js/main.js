ce77dcf724351274703367ff3c32ee87798c96c0
gsilver-p <wldmsdl6276@naver.com>
2025-04-01 17:09:02 +0900

    썸네일과 슬라이드를 넣을 차례

--- static/js/main.js
+++ static/js/main.js
@@ -1,29 +1,39 @@
-document.addEventListener ("DOMContentLoaded", function () {
+document.addEventListener ("DOMContentLoaded", () => {
     const btn = document.getElementById("logo");
     const homeBtn = document.getElementById("home");
+    
     if (btn) {
-      btn.addEventListener ("click", function () {
-        window.location.href="/index.html";
+      btn.addEventListener ("click", () => {
+        window.location.href="index.html";
       })
     }
 
     if (homeBtn) {
-      homeBtn.addEventListener ("click", function () {
-        window.location.href="/index.html";
+      homeBtn.addEventListener ("click", () => {
+        window.location.href="index.html";
       })
     }
   })
 
-  document.addEventListener ("DOMContentLoaded", function () {
+  document.addEventListener ("DOMContentLoaded", () => {
     init()
   })
 
   function init() {
     const menuList = ["profile","project","about"];
-    
-    menuList.forEach((id)=>{
-      document.getElementById(id).addEventListener("click", function () {
-        document.getElementById("sectionContainer").innerHTML = "<div>"+id+"</div>"
-      })
-    })
+    const menuBar = document.querySelectorAll(".side_bar");
+    const container = document.querySelector(".section_container");
+
+    const backgrounds = {
+        home : () => { window.location.href="index.html" },
+        profile : () => {} ,
+        project : () => { style.backgroundColor="rgb(249, 246, 243)"},
+        about : () => {}
+    }
+
+    if( key === "home") {
+        backgrounds.home()
+    } else {
+        document.querySelector(".section_container").style.backgroundImage = backgrounds[key];
+    }
   }
